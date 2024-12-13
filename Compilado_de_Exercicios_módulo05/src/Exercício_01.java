575de9361e8536db8e6b1de5511785b9ded8f9f3
Caroline Bompadre <145167960+CarolineBompadre@users.noreply.github.com>
2024-12-13 15:44:23 -0300

    Compilado Exercícios Seção 05
    Estrutura Comparativa, Lógica, Condicional

---
+++ Compilado_de_Exercicios_módulo05/src/Exercício_01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercício_01 {
+
+	public static void main(String[] args) {
+		// Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		
+		if (A < 0) {
+			System.out.println("Negativo");
+		}
+		else {
+			System.out.println("Positivo");
+		}
+		
+		sc.close();
+		
+	}
+
+}
---
+++ Compilado_de_Exercicios_módulo05/src/Exercício_02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercício_02 {
+
+	public static void main(String[] args) {
+		// Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+		
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+	    
+		if (N % 2 == 0) {
+			System.out.println("PAR");
+		}
+		else {
+			System.out.println("IMPAR");
+		}
+
+		sc.close();
+
+	}
+
+}
